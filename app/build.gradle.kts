a4e337c6aec5241047f01354a8e4e9a68729a228
GaspardD78 <43726973+GaspardD78@users.noreply.github.com>
2025-11-01 13:59:59 +0100

    chore: Update Gradle and dependencies
    This commit updates various Gradle-related files and dependencies to newer versions.
    
    - Bumps the Android Gradle Plugin from 8.2.0 to 8.2.2.
    - Upgrades Kotlin from 1.9.0 to 1.9.22 and KSP to `1.9.22-1.0.17`.
    - Updates multiple AndroidX libraries, including Activity, Lifecycle, Core KTX, and Room.
    - Refreshes the Compose BOM to `2024.02.01`.
    - Adjusts the structure of the `libs.versions.toml` file for better organization.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,7 +71,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
+    androidTestImplementation(libs.androidx.ui.uitest.junit4)
     debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    debugImplementation(libs.androidx.ui.uitest.manifest)
 }
